49a08d8ba42db2d364c00fb2ce90799e5d1122f5
Stiffjobs <52703319+Stiffjobs@users.noreply.github.com>
2020-06-28 01:06:04 +0800

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Chi Chen on 2020/6/28.
+//  Copyright © 2020 Chi Chen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
